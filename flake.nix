1b9ba98bc5c277cc3654b534774d29afc90bdfdb
Łukasz Magnuszewski <lukasz.magnuszewski@gmail.com>
2022-07-11 00:33:08 +0200

    modified:   flake.lock 	modified:   flake.nix 	new file:   home.nix

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   
   inputs = {
 	nixpkgs.url = "nixpkgs/nixos-unstable";
-	#home-manager.url = "github:nix-community/home-manager/master";
-	#home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  	home-manager.url = "github:nix-community/home-manager";
+    	home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = {nixpkgs, ... }:  #home-manager
+  outputs = inputs@{nixpkgs, home-manager, ... }:  
 	let
 		system = "x86_64-linux";
 
@@ -24,7 +24,14 @@
 				inherit system;
 
 				modules = [
-					./configuration.nix					
+					./configuration.nix
+					 home-manager.nixosModules.home-manager {
+            					home-manager.useGlobalPkgs = true;
+            					home-manager.useUserPackages = true;
+            					home-manager.users.sileanth = import ./home.nix;
+            					# Optionally, use home-manager.extraSpecialArgs to pass
+           					 # arguments to home.nix
+          				}
 				];
 			};
 		};
